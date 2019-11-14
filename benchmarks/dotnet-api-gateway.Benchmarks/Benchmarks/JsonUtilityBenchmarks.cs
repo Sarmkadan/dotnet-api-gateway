4d3e249ca07edcad779449d2788b6d4dc5aca20a
Vladyslav Zaiets <rutova2@gmail.com>
2019-11-14 16:40:53 +0200

    JsonUtilityBenchmarks (de)serialization helpers

--- benchmarks/dotnet-api-gateway.Benchmarks/Benchmarks/JsonUtilityBenchmarks.cs
+++ benchmarks/dotnet-api-gateway.Benchmarks/Benchmarks/JsonUtilityBenchmarks.cs
@@ -3,9 +3,15 @@ using DotNetApiGateway.Utilities;
 
 namespace DotNetApiGateway.Benchmarks.Benchmarks
 {
+    /// <summary>
+    /// Benchmark class for JsonUtility.
+    /// </summary>
     [MemoryDiagnoser]
     public class JsonUtilityBenchmarks
     {
+        /// <summary>
+        /// Test class used for serialization and deserialization benchmarks.
+        /// </summary>
         public class TestClass
         {
             public string Name { get; set; } = "Test";
@@ -15,6 +21,9 @@ namespace DotNetApiGateway.Benchmarks.Benchmarks
         private TestClass _obj = null!;
         private string _json = null!;
 
+        /// <summary>
+        /// Global setup method to initialize the test object and JSON string.
+        /// </summary>
         [GlobalSetup]
         public void Setup()
         {
@@ -22,12 +31,20 @@ namespace DotNetApiGateway.Benchmarks.Benchmarks
             _json = JsonUtility.Serialize(_obj);
         }
 
+        /// <summary>
+        /// Benchmark method to measure the serialization time of the test object.
+        /// </summary>
+        /// <returns>The serialized JSON string.</returns>
         [Benchmark]
         public string Serialize()
         {
             return JsonUtility.Serialize(_obj);
         }
 
+        /// <summary>
+        /// Benchmark method to measure the deserialization time of the test object from the JSON string.
+        /// </summary>
+        /// <returns>The deserialized test object.</returns>
         [Benchmark]
         public TestClass? Deserialize()
         {
