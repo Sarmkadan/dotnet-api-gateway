c20f916039c8b8a2bb3a602cfd63d6f1e2cd6780
Vladyslav Zaiets <rutova2@gmail.com>
2019-11-20 07:08:46 +0200

    A couple of WebhookSubscription extensions

---
+++ Models/WebhookSubscriptionExtensions.cs
@@ -0,0 +1,74 @@
+namespace DotNetApiGateway.Models;
+
+/// <summary>
+/// Extension methods for <see cref="WebhookSubscription"/> providing common operations and utilities.
+/// </summary>
+public static class WebhookSubscriptionExtensions
+{
+    /// <summary>
+    /// Determines whether the subscription is currently active and ready to receive events.
+    /// </summary>
+    /// <param name="subscription">The webhook subscription.</param>
+    /// <returns>True if the subscription is active; otherwise, false.</returns>
+    public static bool IsActive(this WebhookSubscription subscription)
+    {
+        ArgumentNullException.ThrowIfNull(subscription);
+        return subscription.Active && subscription.CallbackUrl != null;
+    }
+
+    /// <summary>
+    /// Gets the current delivery success rate as a percentage (0-100).
+    /// </summary>
+    /// <param name="subscription">The webhook subscription.</param>
+    /// <returns>The success rate percentage, or 0 if no deliveries have been attempted.</returns>
+    public static int GetSuccessRate(this WebhookSubscription subscription)
+    {
+        ArgumentNullException.ThrowIfNull(subscription);
+
+        if (subscription.DeliveryStats.TotalDeliveries == 0)
+        {
+            return 0;
+        }
+
+        return (int)Math.Round((double)subscription.DeliveryStats.SuccessfulDeliveries / subscription.DeliveryStats.TotalDeliveries * 100);
+    }
+
+    /// <summary>
+    /// Gets the next retry delay in milliseconds based on the current retry count and policy.
+    /// </summary>
+    /// <param name="subscription">The webhook subscription.</param>
+    /// <param name="currentRetryCount">The number of previous retry attempts.</param>
+    /// <returns>The delay in milliseconds before the next retry attempt.</returns>
+    public static int GetNextRetryDelay(this WebhookSubscription subscription, int currentRetryCount)
+    {
+        ArgumentNullException.ThrowIfNull(subscription);
+
+        if (subscription.RetryPolicy == null || currentRetryCount < 0)
+        {
+            return subscription.RetryPolicy?.InitialDelayMs ?? 1000;
+        }
+
+        // Exponential backoff with jitter
+        var delay = Math.Min(
+            (subscription.RetryPolicy?.InitialDelayMs ?? 1000) * (int)Math.Pow(2, currentRetryCount),
+            subscription.RetryPolicy?.MaxDelayMs ?? 60000
+        );
+
+        // Add jitter: ±20% random variation
+        var random = new Random(subscription.Id.GetHashCode() ^ currentRetryCount);
+        var jitter = (int)(delay * 0.2 * random.NextDouble());
+        return Math.Max(delay - jitter, 100); // Minimum 100ms
+    }
+
+    /// <summary>
+    /// Determines if the subscription has exceeded its maximum retry attempts.
+    /// </summary>
+    /// <param name="subscription">The webhook subscription.</param>
+    /// <param name="currentRetryCount">The number of previous retry attempts.</param>
+    /// <returns>True if the subscription has exceeded its maximum retry attempts; otherwise, false.</returns>
+    public static bool HasExceededMaxRetries(this WebhookSubscription subscription, int currentRetryCount)
+    {
+        ArgumentNullException.ThrowIfNull(subscription);
+        return currentRetryCount >= (subscription.RetryPolicy?.MaxRetries ?? 3);
+    }
+}
