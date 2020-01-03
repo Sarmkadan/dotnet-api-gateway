0229659847230fc550efc045540fafae6d99f08d
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-03 12:41:14 +0200

    feat(exceptions): small fix

--- Exceptions/GatewayExceptionExtensions.cs
+++ Exceptions/GatewayExceptionExtensions.cs
@@ -12,33 +12,42 @@ namespace DotNetApiGateway.Exceptions
         /// <summary>
         /// Determines whether the exception represents a client‑side error (HTTP 4xx).
         /// </summary>
+        /// <param name="ex">The exception to check.</param>
+        /// <returns><see langword="true"/> if the exception represents a client-side error; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static bool IsClientError(this GatewayException ex)
         {
-            if (ex == null) throw new ArgumentNullException(nameof(ex));
-            return ex.StatusCode >= 400 && ex.StatusCode < 500;
+            ArgumentNullException.ThrowIfNull(ex);
+            return ex.StatusCode is >= 400 and < 500;
         }
 
         /// <summary>
         /// Determines whether the exception represents a server‑side error (HTTP 5xx).
         /// </summary>
+        /// <param name="ex">The exception to check.</param>
+        /// <returns><see langword="true"/> if the exception represents a server-side error; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static bool IsServerError(this GatewayException ex)
         {
-            if (ex == null) throw new ArgumentNullException(nameof(ex));
-            return ex.StatusCode >= 500 && ex.StatusCode < 600;
+            ArgumentNullException.ThrowIfNull(ex);
+            return ex.StatusCode is >= 500 and < 600;
         }
 
         /// <summary>
         /// Serialises the exception details (message, error code and status code) to a JSON string.
         /// </summary>
+        /// <param name="ex">The exception to serialize.</param>
+        /// <returns>A JSON string containing the exception details.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static string ToJson(this GatewayException ex)
         {
-            if (ex == null) throw new ArgumentNullException(nameof(ex));
+            ArgumentNullException.ThrowIfNull(ex);
 
             var payload = new
             {
-                ex.Message,
-                ex.ErrorCode,
-                ex.StatusCode
+                Message = ex.Message,
+                ErrorCode = ex.ErrorCode,
+                StatusCode = ex.StatusCode
             };
 
             return JsonSerializer.Serialize(payload);
@@ -48,10 +57,13 @@ namespace DotNetApiGateway.Exceptions
         /// Logs the exception using the supplied <see cref="ILogger"/>.
         /// The log entry includes the message, error code and status code.
         /// </summary>
+        /// <param name="ex">The exception to log.</param>
+        /// <param name="logger">The logger instance to use.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> or <paramref name="logger"/> is <see langword="null"/>.</exception>
         public static void Log(this GatewayException ex, ILogger logger)
         {
-            if (ex == null) throw new ArgumentNullException(nameof(ex));
-            if (logger == null) throw new ArgumentNullException(nameof(logger));
+            ArgumentNullException.ThrowIfNull(ex);
+            ArgumentNullException.ThrowIfNull(logger);
 
             logger.LogError(
                 ex,
