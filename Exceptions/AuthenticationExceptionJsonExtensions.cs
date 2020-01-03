880516632bf9a2bdb669c49ee0ea7c560b39b769
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-03 18:16:25 +0200

    json serialization for AuthenticationEcxeptionJson
    should be backwards compatible.

--- Exceptions/AuthenticationExceptionJsonExtensions.cs
+++ Exceptions/AuthenticationExceptionJsonExtensions.cs
@@ -18,21 +18,20 @@ public static class AuthenticationExceptionJsonExtensions
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
     };
 
     /// <summary>
     /// Serializes the <see cref="AuthenticationException"/> to a JSON string
     /// </summary>
-    /// <param name="value">The exception to serialize</param>
-    /// <param name="indented">Whether to format the JSON with indentation</param>
-    /// <returns>A JSON string representation of the exception</returns>
+    /// <param name="value">The exception to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the exception.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this AuthenticationException value, bool indented = false)
     {
-        if (value is null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+        ArgumentNullException.ThrowIfNull(value);
 
         var options = indented
             ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
@@ -44,10 +43,13 @@ public static class AuthenticationExceptionJsonExtensions
     /// <summary>
     /// Deserializes an <see cref="AuthenticationException"/> from a JSON string
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <returns>The deserialized exception, or null if the JSON is invalid</returns>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized exception, or <see langword="null"/> if the JSON is invalid or whitespace.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static AuthenticationException? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrWhiteSpace(json))
         {
             return null;
@@ -66,11 +68,14 @@ public static class AuthenticationExceptionJsonExtensions
     /// <summary>
     /// Attempts to deserialize an <see cref="AuthenticationException"/> from a JSON string
     /// </summary>
-    /// <param name="json">The JSON string to deserialize</param>
-    /// <param name="value">The deserialized exception, or null if deserialization failed</param>
-    /// <returns>True if deserialization succeeded; false otherwise</returns>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">The deserialized exception, or <see langword="null"/> if deserialization failed.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out AuthenticationException? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         value = null;
 
         if (string.IsNullOrWhiteSpace(json))
