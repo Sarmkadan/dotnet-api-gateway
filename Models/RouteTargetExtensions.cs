72db5114adee26a06726c6bf2ecc673bc5eb3e07
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-07 15:08:58 +0200

    Handy extensions for RouteTarget
    found this while looking at something else.

--- Models/RouteTargetExtensions.cs
+++ Models/RouteTargetExtensions.cs
@@ -11,9 +11,12 @@ public static class RouteTargetExtensions
     /// Gets the effective base URL including port if specified
     /// </summary>
     /// <param name="target">The route target</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="target"/> is null</exception>
     /// <returns>Base URL with port if port is set</returns>
     public static string GetEffectiveBaseUrl(this RouteTarget target)
     {
+        ArgumentNullException.ThrowIfNull(target);
+
         if (target.Port == null || target.Port == 80 || target.Port == 443)
         {
             return target.BaseUrl;
@@ -31,9 +34,12 @@ public static class RouteTargetExtensions
     /// Calculates the effective timeout in milliseconds for this target
     /// </summary>
     /// <param name="target">The route target</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="target"/> is null</exception>
     /// <returns>Timeout in milliseconds, or null if not set</returns>
     public static int? GetTimeoutMilliseconds(this RouteTarget target)
     {
+        ArgumentNullException.ThrowIfNull(target);
+
         return target.TimeoutSeconds.HasValue
             ? target.TimeoutSeconds.Value * 1000
             : null;
@@ -44,9 +50,12 @@ public static class RouteTargetExtensions
     /// </summary>
     /// <param name="target">The route target</param>
     /// <param name="currentTime">Current UTC time to check against health check interval</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="target"/> is null</exception>
     /// <returns>True if target should receive traffic, false otherwise</returns>
     public static bool ShouldReceiveTraffic(this RouteTarget target, DateTime currentTime)
     {
+        ArgumentNullException.ThrowIfNull(target);
+
         if (target.IsHealthy)
         {
             return true;
@@ -66,9 +75,18 @@ public static class RouteTargetExtensions
     /// Gets a normalized weight value for consistent load balancing calculations
     /// </summary>
     /// <param name="target">The route target</param>
-    /// <returns>Normalized weight value</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="target"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="target"/>.Weight is negative</exception>
+    /// <returns>Normalized weight value between 0.0 and 1.0</returns>
     public static double GetNormalizedWeight(this RouteTarget target)
     {
+        ArgumentNullException.ThrowIfNull(target);
+
+        if (target.Weight < 0)
+        {
+            throw new ArgumentException("Weight cannot be negative", nameof(target));
+        }
+
         return target.Weight / 100.0;
     }
 }
