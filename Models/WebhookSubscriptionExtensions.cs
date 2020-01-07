b3783b7b7c8bd010b8013d94a8d413127bafe4da
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-07 09:49:39 +0200

    handy extensions for WebhookSubscription

--- Models/WebhookSubscriptionExtensions.cs
+++ Models/WebhookSubscriptionExtensions.cs
@@ -10,17 +10,19 @@ public static class WebhookSubscriptionExtensions
     /// </summary>
     /// <param name="subscription">The webhook subscription.</param>
     /// <returns>True if the subscription is active; otherwise, false.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="subscription"/> is null.</exception>
     public static bool IsActive(this WebhookSubscription subscription)
     {
         ArgumentNullException.ThrowIfNull(subscription);
-        return subscription.Active && subscription.CallbackUrl != null;
+        return subscription.Active && !string.IsNullOrEmpty(subscription.CallbackUrl);
     }
 
     /// <summary>
     /// Gets the current delivery success rate as a percentage (0-100).
     /// </summary>
     /// <param name="subscription">The webhook subscription.</param>
-    /// <returns>The success rate percentage, or 0 if no deliveries have been attempted.</returns>
+    /// <returns>The success rate percentage, or 0 if no deliveries have been attempted or <paramref name="subscription"/> is null.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="subscription"/> is null.</exception>
     public static int GetSuccessRate(this WebhookSubscription subscription)
     {
         ArgumentNullException.ThrowIfNull(subscription);
@@ -39,23 +41,28 @@ public static class WebhookSubscriptionExtensions
     /// <param name="subscription">The webhook subscription.</param>
     /// <param name="currentRetryCount">The number of previous retry attempts.</param>
     /// <returns>The delay in milliseconds before the next retry attempt.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="subscription"/> is null.</exception>
+ /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="currentRetryCount"/> is negative.</exception>
     public static int GetNextRetryDelay(this WebhookSubscription subscription, int currentRetryCount)
     {
         ArgumentNullException.ThrowIfNull(subscription);
 
-        if (subscription.RetryPolicy == null || currentRetryCount < 0)
+        ArgumentOutOfRangeException.ThrowIfNegative(currentRetryCount);
+
+    if (subscription.RetryPolicy is null)
         {
-            return subscription.RetryPolicy?.InitialDelayMs ?? 1000;
+            return 1000;
         }
 
         // Exponential backoff with jitter
         var delay = Math.Min(
-            (subscription.RetryPolicy?.InitialDelayMs ?? 1000) * (int)Math.Pow(2, currentRetryCount),
-            subscription.RetryPolicy?.MaxDelayMs ?? 60000
+            subscription.RetryPolicy.InitialDelayMs * (int)Math.Pow(2, currentRetryCount),
+            subscription.RetryPolicy.MaxDelayMs
         );
 
         // Add jitter: ±20% random variation
-        var random = new Random(subscription.Id.GetHashCode() ^ currentRetryCount);
+        // Use thread-safe Random.Shared to avoid issues with rapid successive calls
+    var random = Random.Shared;
         var jitter = (int)(delay * 0.2 * random.NextDouble());
         return Math.Max(delay - jitter, 100); // Minimum 100ms
     }
@@ -66,6 +73,7 @@ public static class WebhookSubscriptionExtensions
     /// <param name="subscription">The webhook subscription.</param>
     /// <param name="currentRetryCount">The number of previous retry attempts.</param>
     /// <returns>True if the subscription has exceeded its maximum retry attempts; otherwise, false.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="subscription"/> is null.</exception>
     public static bool HasExceededMaxRetries(this WebhookSubscription subscription, int currentRetryCount)
     {
         ArgumentNullException.ThrowIfNull(subscription);
