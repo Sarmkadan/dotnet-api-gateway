b50404d6f9a2fc621efe6dc3ad3156abd3f6595e
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-09 08:09:26 +0200

    serialize CircuitBreakerRepositoryJson properly

--- Repositories/CircuitBreakerRepositoryJsonExtensions.cs
+++ Repositories/CircuitBreakerRepositoryJsonExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 
 namespace DotNetApiGateway.Repositories
@@ -8,19 +9,22 @@ namespace DotNetApiGateway.Repositories
     public static class CircuitBreakerRepositoryJsonExtensions
     {
         // Cached serializer options with camel‑case naming.
-        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web)
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         };
 
         /// <summary>
-        /// Serialises the repository to a JSON string.
+        /// Serializes the repository to a JSON string.
         /// </summary>
-        /// <param name="value">The repository instance to serialise.</param>
+        /// <param name="value">The repository instance to serialize.</param>
         /// <param name="indented">If <c>true</c>, the output will be indented.</param>
         /// <returns>A JSON representation of the repository.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <c>null</c>.</exception>
         public static string ToJson(this CircuitBreakerRepository value, bool indented = false)
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             var options = indented
                 ? new JsonSerializerOptions(_options) { WriteIndented = true }
                 : _options;
@@ -29,26 +33,36 @@ namespace DotNetApiGateway.Repositories
         }
 
         /// <summary>
-        /// Deserialises a JSON string into a <see cref="CircuitBreakerRepository"/> instance.
+        /// Deserializes a JSON string into a <see cref="CircuitBreakerRepository"/> instance.
         /// </summary>
         /// <param name="json">The JSON string.</param>
-        /// <returns>The deserialised repository, or <c>null</c> if the JSON is <c>null</c> or empty.</returns>
+        /// <returns>The deserialized repository, or <c>null</c> if the JSON is <c>null</c>, empty, or whitespace.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
         public static CircuitBreakerRepository? FromJson(string json)
         {
-            return JsonSerializer.Deserialize<CircuitBreakerRepository>(json, _options);
+            ArgumentNullException.ThrowIfNull(json);
+
+            return string.IsNullOrWhiteSpace(json)
+                ? null
+                : JsonSerializer.Deserialize<CircuitBreakerRepository>(json, _options);
         }
 
         /// <summary>
-        /// Attempts to deserialise a JSON string into a <see cref="CircuitBreakerRepository"/> instance.
+        /// Attempts to deserialize a JSON string into a <see cref="CircuitBreakerRepository"/> instance.
         /// </summary>
         /// <param name="json">The JSON string.</param>
-        /// <param name="value">When the method returns, contains the deserialised repository if successful; otherwise <c>null</c>.</param>
+        /// <param name="value">When the method returns, contains the deserialized repository if successful; otherwise <c>null</c>.</param>
         /// <returns><c>true</c> if deserialization succeeded; otherwise <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <c>null</c>.</exception>
         public static bool TryFromJson(string json, out CircuitBreakerRepository? value)
         {
+            ArgumentNullException.ThrowIfNull(json);
+
             try
             {
-                value = JsonSerializer.Deserialize<CircuitBreakerRepository>(json, _options);
+                value = string.IsNullOrWhiteSpace(json)
+                    ? null
+                    : JsonSerializer.Deserialize<CircuitBreakerRepository>(json, _options);
                 return true;
             }
             catch (JsonException)
