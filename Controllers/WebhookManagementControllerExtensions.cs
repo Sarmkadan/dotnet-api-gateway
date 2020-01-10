f748ecd8fe7a94b2843fae81981963f3fdfde569
Vladyslav Zaiets <rutova2@gmail.com>
2020-01-10 19:45:08 +0200

    add extension helpers for WebhookManagementController
    mostly WebhookManagementControllerExtensions.cs, nothing exciting.

--- Controllers/WebhookManagementControllerExtensions.cs
+++ Controllers/WebhookManagementControllerExtensions.cs
@@ -4,6 +4,7 @@ namespace DotNetApiGateway.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
 using DotNetApiGateway.Models;
+using System;
 using System.Linq;
 
 /// <summary>
@@ -17,25 +18,31 @@ public static class WebhookManagementControllerExtensions
     /// <param name="controller">The webhook management controller instance.</param>
     /// <param name="eventType">The event type to filter by (use "*" for all).</param>
     /// <returns>Filtered list of webhook subscriptions.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="eventType"/> is <see langword="null"/>.</exception>
     public static IActionResult GetWebhookSubscriptionsByEventType(this WebhookManagementController controller, string eventType)
     {
-        if (controller is null)
-            throw new ArgumentNullException(nameof(controller));
+        ArgumentNullException.ThrowIfNull(controller);
+        ArgumentNullException.ThrowIfNull(eventType);
 
         var allSubscriptions = controller.GetAllWebhookSubscriptions();
 
-        if (allSubscriptions is not OkObjectResult okResult)
-            return allSubscriptions;
-
-        var subscriptions = (IEnumerable<WebhookSubscription>)okResult.Value!;
-
-        var filteredSubscriptions = eventType switch
+        return allSubscriptions switch
         {
-            "*" => subscriptions,
-            _ => subscriptions.Where(s => s.EventTypes.Contains(eventType) || s.EventTypes.Contains("*")).ToList()
+            OkObjectResult okResult => HandleGetSubscriptionsByEventType(okResult, eventType),
+            _ => allSubscriptions
         };
 
-        return new OkObjectResult(filteredSubscriptions);
+        static IActionResult HandleGetSubscriptionsByEventType(OkObjectResult okResult, string eventType)
+        {
+            var subscriptions = (IEnumerable<WebhookSubscription>)okResult.Value!;
+            var filteredSubscriptions = eventType switch
+            {
+                "*" => subscriptions,
+                _ => subscriptions.Where(s => s.EventTypes.Contains(eventType) || s.EventTypes.Contains("*")).ToList()
+            };
+            return new OkObjectResult(filteredSubscriptions);
+        }
     }
 
     /// <summary>
@@ -44,25 +51,33 @@ public static class WebhookManagementControllerExtensions
     /// <param name="controller">The webhook management controller instance.</param>
     /// <param name="id">The subscription ID to pause.</param>
     /// <returns>No content result.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="id"/> is <see langword="null"/>.</exception>
     public static IActionResult PauseWebhookSubscription(this WebhookManagementController controller, string id)
     {
-        if (controller is null)
-            throw new ArgumentNullException(nameof(controller));
+        ArgumentNullException.ThrowIfNull(controller);
+        ArgumentNullException.ThrowIfNull(id);
 
         var subscriptionResult = controller.GetWebhookSubscription(id);
 
-        if (subscriptionResult is not OkObjectResult okResult)
-            return subscriptionResult;
-
-        var subscription = (WebhookSubscription)okResult.Value!;
-        subscription.Active = false;
-
-        var updateRequest = new UpdateWebhookSubscriptionRequest
+        return subscriptionResult switch
         {
-            EventTypes = subscription.EventTypes
+            OkObjectResult okResult => HandlePauseSubscription(controller, okResult, id),
+            _ => subscriptionResult
         };
 
-        return controller.UpdateWebhookSubscription(id, updateRequest);
+        static IActionResult HandlePauseSubscription(WebhookManagementController controller, OkObjectResult okResult, string id)
+        {
+            var subscription = (WebhookSubscription)okResult.Value!;
+            subscription.Active = false;
+
+            var updateRequest = new UpdateWebhookSubscriptionRequest
+            {
+                EventTypes = subscription.EventTypes
+            };
+
+            return controller.UpdateWebhookSubscription(id, updateRequest);
+        }
     }
 
     /// <summary>
@@ -71,25 +86,33 @@ public static class WebhookManagementControllerExtensions
     /// <param name="controller">The webhook management controller instance.</param>
     /// <param name="id">The subscription ID to resume.</param>
     /// <returns>No content result.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="id"/> is <see langword="null"/>.</exception>
     public static IActionResult ResumeWebhookSubscription(this WebhookManagementController controller, string id)
     {
-        if (controller is null)
-            throw new ArgumentNullException(nameof(controller));
+        ArgumentNullException.ThrowIfNull(controller);
+        ArgumentNullException.ThrowIfNull(id);
 
         var subscriptionResult = controller.GetWebhookSubscription(id);
 
-        if (subscriptionResult is not OkObjectResult okResult)
-            return subscriptionResult;
-
-        var subscription = (WebhookSubscription)okResult.Value!;
-        subscription.Active = true;
-
-        var updateRequest = new UpdateWebhookSubscriptionRequest
+        return subscriptionResult switch
         {
-            EventTypes = subscription.EventTypes
+            OkObjectResult okResult => HandleResumeSubscription(controller, okResult, id),
+            _ => subscriptionResult
         };
 
-        return controller.UpdateWebhookSubscription(id, updateRequest);
+        static IActionResult HandleResumeSubscription(WebhookManagementController controller, OkObjectResult okResult, string id)
+        {
+            var subscription = (WebhookSubscription)okResult.Value!;
+            subscription.Active = true;
+
+            var updateRequest = new UpdateWebhookSubscriptionRequest
+            {
+                EventTypes = subscription.EventTypes
+            };
+
+            return controller.UpdateWebhookSubscription(id, updateRequest);
+        }
     }
 
     /// <summary>
@@ -99,27 +122,37 @@ public static class WebhookManagementControllerExtensions
     /// <param name="id">The subscription ID to update.</param>
     /// <param name="maxRetries">Maximum number of retry attempts.</param>
     /// <returns>The updated subscription.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="controller"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="id"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="maxRetries"/> is negative.</exception>
     public static IActionResult UpdateWebhookRetryPolicy(
         this WebhookManagementController controller,
         string id,
         int maxRetries)
     {
-        if (controller is null)
-            throw new ArgumentNullException(nameof(controller));
+        ArgumentNullException.ThrowIfNull(controller);
+        ArgumentNullException.ThrowIfNull(id);
+        ArgumentOutOfRangeException.ThrowIfNegative(maxRetries);
 
         var subscriptionResult = controller.GetWebhookSubscription(id);
 
-        if (subscriptionResult is not OkObjectResult okResult)
-            return subscriptionResult;
-
-        var subscription = (WebhookSubscription)okResult.Value!;
-
-        var updateRequest = new UpdateWebhookSubscriptionRequest
+        return subscriptionResult switch
         {
-            EventTypes = subscription.EventTypes,
-            MaxRetries = maxRetries
+            OkObjectResult okResult => HandleUpdateRetryPolicy(controller, okResult, id, maxRetries),
+            _ => subscriptionResult
         };
 
-        return controller.UpdateWebhookSubscription(id, updateRequest);
+        static IActionResult HandleUpdateRetryPolicy(WebhookManagementController controller, OkObjectResult okResult, string id, int maxRetries)
+        {
+            var subscription = (WebhookSubscription)okResult.Value!;
+
+            var updateRequest = new UpdateWebhookSubscriptionRequest
+            {
+                EventTypes = subscription.EventTypes,
+                MaxRetries = maxRetries
+            };
+
+            return controller.UpdateWebhookSubscription(id, updateRequest);
+        }
     }
 }
