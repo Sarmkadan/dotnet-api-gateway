4dcdc018dda0ca274ac761eb6f60ccf4c5292240
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 13:21:21 +0300

    readability pass over ValidationUtilityTestsExtensions

--- tests/dotnet-api-gateway.Tests/ValidationUtilityTestsExtensions.cs
+++ tests/dotnet-api-gateway.Tests/ValidationUtilityTestsExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using DotNetApiGateway.Utilities;
 using FluentAssertions;
@@ -18,112 +18,113 @@ namespace DotNetApiGateway.Tests;
 /// </summary>
 public static class ValidationUtilityTestsExtensions
 {
-    /// <summary>
-    /// Validates that a collection contains only valid email addresses.
-    /// </summary>
-    /// <param name="test">The test instance.</param>
-    /// <param name="emails">The collection of email addresses to validate.</param>
-    /// <param name="expectedInvalidCount">The expected number of invalid emails in the collection.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="emails"/> is null.</exception>
-    public static void AllEmailsValid(
-        this ValidationUtilityTests test,
-        [NotNull] IEnumerable<string> emails,
-        int expectedInvalidCount = 0)
-    {
-        ArgumentNullException.ThrowIfNull(emails);
+	/// <summary>
+	/// Validates that a collection contains only valid email addresses.
+	/// </summary>
+	/// <param name="test">The test instance.</param>
+	/// <param name="emails">The collection of email addresses to validate.</param>
+	/// <param name="expectedInvalidCount">The expected number of invalid emails in the collection.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="emails"/> is null.</exception>
+	public static void AllEmailsValid(
+		this ValidationUtilityTests test,
+		[NotNull] IEnumerable<string> emails,
+		int expectedInvalidCount = 0)
+	{
+		ArgumentNullException.ThrowIfNull(emails);
 
-        var invalidEmails = new List<string>();
-        var index = 0;
+		var invalidEmails = new List<string>();
+		var index = 0;
 
-        foreach (var email in emails)
-        {
-            if (!ValidationUtility.IsValidEmail(email))
-            {
-                invalidEmails.Add($"Index {index}: '{email}'");
-            }
-            index++;
-        }
+		foreach (var email in emails)
+		{
+			if (!ValidationUtility.IsValidEmail(email))
+			{
+				invalidEmails.Add($"Index {index}: '{email}'");
+			}
+			index++;
+		}
 
-        invalidEmails.Should().HaveCount(expectedInvalidCount,
-            $"Expected {expectedInvalidCount} invalid emails but found {invalidEmails.Count}. Invalid emails: {string.Join(", ", invalidEmails)}");
-    }
+		invalidEmails.Should().HaveCount(expectedInvalidCount,
+			$"Expected {expectedInvalidCount} invalid emails but found {invalidEmails.Count}. Invalid emails: {string.Join(", ", invalidEmails)}");
+	}
 
-    /// <summary>
-    /// Validates that a collection contains only valid URLs.
-    /// </summary>
-    /// <param name="test">The test instance.</param>
-    /// <param name="urls">The collection of URLs to validate.</param>
-    /// <param name="expectedInvalidCount">The expected number of invalid URLs in the collection.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="urls"/> is null.</exception>
-    public static void AllUrlsValid(
-        this ValidationUtilityTests test,
-        [NotNull] IEnumerable<string> urls,
-        int expectedInvalidCount = 0)
-    {
-        ArgumentNullException.ThrowIfNull(urls);
+	/// <summary>
+	/// Validates that a collection contains only valid URLs.
+	/// </summary>
+	/// <param name="test">The test instance.</param>
+	/// <param name="urls">The collection of URLs to validate.</param>
+	/// <param name="expectedInvalidCount">The expected number of invalid URLs in the collection.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="urls"/> is null.</exception>
+	public static void AllUrlsValid(
+		this ValidationUtilityTests test,
+		[NotNull] IEnumerable<string> urls,
+		int expectedInvalidCount = 0)
+	{
+		ArgumentNullException.ThrowIfNull(urls);
 
-        var invalidUrls = new List<string>();
-        var index = 0;
+		var invalidUrls = new List<string>();
+		var index = 0;
 
-        foreach (var url in urls)
-        {
-            if (!ValidationUtility.IsValidUrl(url))
-            {
-                invalidUrls.Add($"Index {index}: '{url}'");
-            }
-            index++;
-        }
+		foreach (var url in urls)
+		{
+			if (!ValidationUtility.IsValidUrl(url))
+			{
+				invalidUrls.Add($"Index {index}: '{url}'");
+			}
+			index++;
+		}
 
-        invalidUrls.Should().HaveCount(expectedInvalidCount,
-            $"Expected {expectedInvalidCount} invalid URLs but found {invalidUrls.Count}. Invalid URLs: {string.Join(", ", invalidUrls)}");
-    }
+		invalidUrls.Should().HaveCount(expectedInvalidCount,
+			$"Expected {expectedInvalidCount} invalid URLs but found {invalidUrls.Count}. Invalid URLs: {string.Join(", ", invalidUrls)}");
+	}
 
-    /// <summary>
-    /// Validates that a collection of key-value pairs contains all required keys.
-    /// </summary>
-    /// <param name="test">The test instance.</param>
-    /// <param name="dictionary">The dictionary to validate.</param>
-    /// <param name="requiredKeys">The required keys that must be present.</param>
-    /// <returns>True if all required keys are present; otherwise false.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> or <paramref name="requiredKeys"/> is null.</exception>
-    public static bool HasAllRequiredKeys(
-        this ValidationUtilityTests test,
-        [NotNull] Dictionary<string, string?> dictionary,
-        [NotNull] params string[] requiredKeys)
-    {
-        ArgumentNullException.ThrowIfNull(dictionary);
-        ArgumentNullException.ThrowIfNull(requiredKeys);
+	/// <summary>
+	/// Validates that a collection of key-value pairs contains all required keys.
+	/// </summary>
+	/// <param name="test">The test instance.</param>
+	/// <param name="dictionary">The dictionary to validate.</param>
+	/// <param name="requiredKeys">The required keys that must be present.</param>
+	/// <returns>True if all required keys are present; otherwise false.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is null.</exception>
+	/// <exception cref="ArgumentNullException"><paramref name="requiredKeys"/> is null.</exception>
+	public static bool HasAllRequiredKeys(
+		this ValidationUtilityTests test,
+		[NotNull] Dictionary<string, string?> dictionary,
+		[NotNull] params string[] requiredKeys)
+	{
+		ArgumentNullException.ThrowIfNull(dictionary);
+		ArgumentNullException.ThrowIfNull(requiredKeys);
 
-        return ValidationUtility.HasRequiredKeys(dictionary, requiredKeys);
-    }
+		return ValidationUtility.HasRequiredKeys(dictionary, requiredKeys);
+	}
 
-    /// <summary>
-    /// Validates that a collection of strings are all alphanumeric.
-    /// </summary>
-    /// <param name="test">The test instance.</param>
-    /// <param name="values">The collection of strings to validate.</param>
-    /// <param name="expectedInvalidCount">The expected number of invalid alphanumeric strings.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="values"/> is null.</exception>
-    public static void AllAlphanumeric(
-        this ValidationUtilityTests test,
-        [NotNull] IEnumerable<string> values,
-        int expectedInvalidCount = 0)
-    {
-        ArgumentNullException.ThrowIfNull(values);
+	/// <summary>
+	/// Validates that a collection of strings are all alphanumeric.
+	/// </summary>
+	/// <param name="test">The test instance.</param>
+	/// <param name="values">The collection of strings to validate.</param>
+	/// <param name="expectedInvalidCount">The expected number of invalid alphanumeric strings.</param>
+	/// <exception cref="ArgumentNullException"><paramref name="values"/> is null.</exception>
+	public static void AllAlphanumeric(
+		this ValidationUtilityTests test,
+		[NotNull] IEnumerable<string> values,
+		int expectedInvalidCount = 0)
+	{
+		ArgumentNullException.ThrowIfNull(values);
 
-        var invalidValues = new List<string>();
-        var index = 0;
+		var invalidValues = new List<string>();
+		var index = 0;
 
-        foreach (var value in values)
-        {
-            if (!ValidationUtility.IsAlphanumeric(value))
-            {
-                invalidValues.Add($"Index {index}: '{value}'");
-            }
-            index++;
-        }
+		foreach (var value in values)
+		{
+			if (!ValidationUtility.IsAlphanumeric(value))
+			{
+				invalidValues.Add($"Index {index}: '{value}'");
+			}
+			index++;
+		}
 
-        invalidValues.Should().HaveCount(expectedInvalidCount,
-            $"Expected {expectedInvalidCount} invalid alphanumeric strings but found {invalidValues.Count}. Invalid values: {string.Join(", ", invalidValues)}");
-    }
+		invalidValues.Should().HaveCount(expectedInvalidCount,
+			$"Expected {expectedInvalidCount} invalid alphanumeric strings but found {invalidValues.Count}. Invalid values: {string.Join(", ", invalidValues)}");
+	}
 }
