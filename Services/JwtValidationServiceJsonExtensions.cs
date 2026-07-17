562a7ccbee9c059401224895465f4652ab14b030
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Cleanup in JwtValidationServiceJsonExtensions

--- Services/JwtValidationServiceJsonExtensions.cs
+++ Services/JwtValidationServiceJsonExtensions.cs
@@ -15,71 +15,77 @@ using System.Text.Json.Serialization;
 /// </summary>
 public static class JwtValidationServiceJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-        PropertyNameCaseInsensitive = true
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+		PropertyNameCaseInsensitive = true
+	};
 
-    /// <summary>
-    /// Serializes the <see cref="JwtValidationService"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The service instance to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the service.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this JwtValidationService value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
+	/// <summary>
+	/// Serializes the <see cref="JwtValidationService"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The service instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the service.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+	public static string ToJson(this JwtValidationService value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="JwtValidationService"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized service instance, or null if the JSON is null or empty.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static JwtValidationService? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="JwtValidationService"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized service instance, or null if the JSON is null or empty.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static JwtValidationService? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        return JsonSerializer.Deserialize<JwtValidationService>(json, _jsonOptions);
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="JwtValidationService"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized service instance if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out JwtValidationService? value)
-    {
-        value = null;
+		return JsonSerializer.Deserialize<JwtValidationService>(json, _jsonOptions);
+	}
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="JwtValidationService"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized service instance if successful.</param>
+	/// <returns>True if deserialization succeeded; otherwise, false.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+	public static bool TryFromJson(string json, out JwtValidationService? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            value = JsonSerializer.Deserialize<JwtValidationService>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		value = null;
+
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return false;
+		}
+
+		try
+		{
+			value = JsonSerializer.Deserialize<JwtValidationService>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
