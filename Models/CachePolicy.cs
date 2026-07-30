2c7dc8656e17a79115915c41e7a2121b52b38075
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 19:54:10 +0300

    Tweak CachePolicy

--- Models/CachePolicy.cs
+++ Models/CachePolicy.cs
@@ -1,4 +1,6 @@
 #nullable enable
+using System.Linq;
+
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
@@ -9,7 +11,7 @@ namespace DotNetApiGateway.Models;
 /// <summary>
 /// Defines caching behavior for route responses
 /// </summary>
-public sealed class CachePolicy
+public sealed class CachePolicy : IEquatable<CachePolicy>
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public bool Enabled { get; set; } = false;
@@ -21,6 +23,43 @@ public sealed class CachePolicy
     public bool VaryByHeaders { get; set; } = false;
     public string[] VaryHeaders { get; set; } = [];
     public int MaxEntriesInCache { get; set; } = 1000;
+
+    public bool Equals(CachePolicy? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        return Id == other.Id &&
+               Enabled == other.Enabled &&
+               DurationSeconds == other.DurationSeconds &&
+               Strategy == other.Strategy &&
+               CacheableStatusCodes.SequenceEqual(other.CacheableStatusCodes) &&
+               CacheableHttpMethods.SequenceEqual(other.CacheableHttpMethods) &&
+               VaryByQueryString == other.VaryByQueryString &&
+               VaryByHeaders == other.VaryByHeaders;
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as CachePolicy);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        hash.Add(Id);
+        hash.Add(Enabled);
+        hash.Add(DurationSeconds);
+        hash.Add(Strategy);
+        foreach (var code in CacheableStatusCodes) hash.Add(code);
+        foreach (var method in CacheableHttpMethods) hash.Add(method);
+        hash.Add(VaryByQueryString);
+        hash.Add(VaryByHeaders);
+        return hash.ToHashCode();
+    }
+
+    public static bool operator ==(CachePolicy? left, CachePolicy? right) =>
+        Equals(left, right);
+
+    public static bool operator !=(CachePolicy? left, CachePolicy? right) =>
+        !Equals(left, right);
 
     public void Validate()
     {
